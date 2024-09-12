0f8b97434ad1dde37c1707d5b0f2c8c9d7aa2e97
olyavorsina <150282472+olyavorsina@users.noreply.github.com>
2024-09-12 21:48:01 +0500

    Add files via upload

---
+++ Ворсина ОА.py
@@ -0,0 +1,57 @@
+import math
+#def tasks1(a, b, c, d):
+    #N = 2 ** (a / (b * c * d))
+    #return int(N)
+
+#if __name__ == "__main__":
+    #inp1 = int(input("Введите количество бит: "))
+    #inp2 = int(input("Введите количество страниц: "))
+    #inp3 = int(input("Введите количество строк: "))
+    #inp4 = int(input("Введите количество символов: "))
+    #print(tasks1(inp1, inp2, inp3, inp4))
+
+#def tasks2(a, b):
+        #i = (math.log2(a))  / (math.log2(b))
+        #return float(i)
+
+#if __name__ == "__main__":
+    #inp1 = float(input("Введите данные первого текста: "))
+    #inp2 = float(input("Введите данные второго текста: "))
+    #print(tasks2(inp1, inp2))
+
+#def tasks3(b, c):
+    #i = b / (math.log2(c))
+    #return int (i)
+
+#if __name__ == "__main__":
+    #inp1 = int (input("Введите количество бит: "))
+    #inp2 = int (input("Введите количество символов: "))
+    #print(tasks3(inp1, inp2))
+
+#def tasks4(b):
+    #N = 2 ** b
+    #return int (N)
+
+#if __name__ == "__main__":
+    #inp1 = int (input("Сколько бит: "))
+    #print(tasks4(inp1))
+
+#def tasks5(a):
+    #N = 2 ** a
+    #return int (N)
+
+#if __name__ == "__main__":
+    #inp1 = int (input("Сколько бит: "))
+    #print(tasks(inp1))
+
+#def tasks6(a, b, c):
+    #g = (a * b) / (2 ** c)
+    #return int(g)
+#if __name__ == "__main__":
+    #inp1 = int(input("Количество белой краски: "))
+    #inp2 = int(input("Количество синей краски: "))
+    #inp3 = int(input("Количество бит: "))
+    #print(task7(inp1, inp2, inp3))
+
+
+
